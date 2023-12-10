37e475883c2b5f08d7324de327643e481b2fde1a
Dan Lungaro <Chunk_77@hotmail.com>
2023-12-09 16:31:32 -0800

    zeeg and ploog added

--- src/monster_cards/MonsterCharacterFactory.gd
+++ src/monster_cards/MonsterCharacterFactory.gd
@@ -7,5 +7,9 @@ static func get_monster_character(card_name: MonsterCard.CardName) -> MonsterCha
 			return ScenePaths.slaks_slugger_packed_scene.instantiate()
 		MonsterCard.CardName.GLURM:
 			return ScenePaths.glurm_packed_scene.instantiate()
+		MonsterCard.CardName.PLOOG:
+			return ScenePaths.ploog_packed_scene.instantiate()
+		MonsterCard.CardName.ZEEG:
+			return ScenePaths.zeeg_packed_scene.instantiate()
 		_:
 			return ScenePaths.slaks_slugger_packed_scene.instantiate()
